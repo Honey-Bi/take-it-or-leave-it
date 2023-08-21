783ca0703b77b3f3473862df13571d1277e8d964
Honey-Bi <biten10@naver.com>
2023-08-21 14:18:10 +0900

    회원가입용 페이지 생성

---
+++ frontend/src/pages/Sign.tsx
@@ -0,0 +1,16 @@
+export function SignIn() {
+  return (
+    <div className="signIn">
+      asd
+      <button>asd</button>
+    </div>
+  );
+}
+export function SignUp() {
+  return (
+    <div className="signUp">
+      qwe
+      <button>asd</button>
+    </div>
+  );
+}
