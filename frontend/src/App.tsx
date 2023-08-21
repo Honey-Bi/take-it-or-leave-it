edb59955c0d628cdf0fd72a89668bee24b6950db
Honey-Bi <biten10@naver.com>
2023-08-21 16:56:25 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Header from "components/Header";
 import Main from "pages/Main";
 import { SignIn, SignUp } from "pages/Sign";
+import Footer from "components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/sign_in" element={<SignIn />} />
-        <Route path="/sign_up" element={<SignUp />} />
-      </Routes>
-    </>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/sign_in" element={<SignIn />} />
+          <Route path="/sign_up" element={<SignUp />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
