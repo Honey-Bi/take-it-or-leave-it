dc994fa53442186c5ada7997f4bbb0a18e039bb3
Honey-Bi <biten10@naver.com>
2023-08-21 16:56:47 +0900

    Create Select.tsx

---
+++ frontend/src/pages/Select.tsx
@@ -0,0 +1,15 @@
+import style from "./css/main.module.css";
+
+export default function Select() {
+  return (
+    <div className={style.content}>
+      <div className={style.stats}>a</div>
+      <div className={style.dialog}>a</div>
+      <div className={style.deck}></div>
+      <div className={style.name}>
+        test
+        {false ? <span className={style.subname}></span> : ""}
+      </div>
+    </div>
+  );
+}
