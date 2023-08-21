6428869ff7afe2d0a1a4264e2c8108fe6d82521c
Honey-Bi <biten10@naver.com>
2023-08-21 16:56:56 +0900

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,25 +2,30 @@ import { Link } from "react-router-dom";
 import style from "./css/header.module.css";
 
 export default function Header() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  // 화면 크기변경 시
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return (
     <header>
-      <div className="container">
-        <nav>
-          <div className={style.link_home}>
-            <Link to={"/"} className={style.link}>
-              Home
-              <div className={style.underline} />
-            </Link>
-          </div>
-          <div className={style.link_group}>
-            <Link to={"/sign_in"} className={style.link}>
-              Login
-            </Link>
-            <Link to={"/sign_up"} className={style.link}>
-              Sign Up
-            </Link>
-          </div>
-        </nav>
+      <div className={style.link_home}>
+        <Link to={"/"} className={style.link}>
+          Home
+          <div className={style.underline} />
+        </Link>
+      </div>
+      <div className={style.link_group}>
+        <Link to={"/sign_in"} className={style.link}>
+          Login
+        </Link>
+        <Link to={"/sign_up"} className={style.link}>
+          Sign Up
+        </Link>
       </div>
     </header>
   );
