116b31134ac98aca903a9204177432142ae07e77
Honey-Bi <biten10@naver.com>
2023-08-21 14:52:39 +0900

    Update Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import style from "./css/header.module.css";
+
 export default function Header() {
   return (
     <header>
-      <nav></nav>
+      <div className="container">
+        <nav>
+          <div className={style.link_home}>
+            <Link to={"/"} className={style.link}>
+              Home
+              <div className={style.underline} />
+            </Link>
+          </div>
+          <div className={style.link_group}>
+            <Link to={"/sign_in"} className={style.link}>
+              Login
+            </Link>
+            <Link to={"/sign_up"} className={style.link}>
+              Sign Up
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
